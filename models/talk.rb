2e51cd75bc9a59845a6d32fed3dd569917f2e24e
Danilo Barion Nogueira <danilo.barion1986@live.com>
2016-04-05 12:07:05 -0300

    Create talk.rb

---
+++ models/talk.rb
@@ -0,0 +1,20 @@
+class Talk
+	attr_reader 	:title, :duration
+	attr_accessor	:start_time
+		
+	def initialize(talk)
+		@title = title_of(talk)
+		@duration = duration_of(talk)
+		@start_time = ""
+	end
+
+	private
+		def title_of(talk)
+			talk
+		end
+
+		def duration_of(talk)
+			return talk[-5..-4].to_i unless talk.include?("lightning")
+			return 5
+		end
+end
